d1ca2a513929c57fad4923f5a35cbb8985c30c94
afoo <i@afoo.me>
2023-07-11 12:30:43 +0800

    packup

--- src/main/java/com/keevol/kate/Kate.java
+++ src/main/java/com/keevol/kate/Kate.java
@@ -1,21 +1,22 @@
 package com.keevol.kate;
 
-import com.keevol.kate.handlers.SamplePageHandler;
 import com.keevol.kate.templates.jte.JteTemplateEngineFactory;
+import com.keevol.kate.templates.jte.JteTemplateUtils;
+import com.keevol.kate.utils.ResponseUtils;
 import gg.jte.TemplateEngine;
 import io.vertx.core.Future;
 import io.vertx.core.Vertx;
 import io.vertx.core.http.HttpMethod;
 import io.vertx.core.http.HttpServer;
 import io.vertx.ext.web.Router;
-import io.vertx.ext.web.RoutingContext;
 import io.vertx.ext.web.handler.BodyHandler;
 import io.vertx.ext.web.handler.CorsHandler;
-import org.apache.commons.lang3.exception.ExceptionUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.File;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.concurrent.atomic.AtomicLong;
 
@@ -40,7 +41,6 @@ import java.util.concurrent.atomic.AtomicLong;
 public class Kate {
     protected Logger logger = LoggerFactory.getLogger(Kate.class);
 
-    protected KateHandler[] handlers;
     protected RouteRegister[] routeRegisters;
     protected Vertx vertx;
     protected HttpServer httpServer;
@@ -51,32 +51,14 @@ public class Kate {
 
     private String fileLocation = "kate-uploads";
 
-    public Kate(KateHandler[] handlers) {
-        this(Vertx.vertx(), handlers);
+
+    public Kate(RouteRegister[] routeRegisters) {
+        this(Vertx.vertx(), routeRegisters);
         this.implicitVertxCreated = true;
     }
 
-    /**
-     * 2 ways to offer route handlers:
-     *
-     * 1. KateHandler will be handled async with virtual thread by framework, so users don't need to care about the thread blocking things.
-     * 2. RouteRegister give more freedom to users, and they should handle thread blocking things by themselves, otherwise, the performance will suck.
-     *
-     * @param handlers
-     * @param routeRegisters
-     */
-    public Kate(KateHandler[] handlers, RouteRegister[] routeRegisters) {
-        this(Vertx.vertx(), handlers, routeRegisters);
-        this.implicitVertxCreated = true;
-    }
-
-    public Kate(Vertx vertx, KateHandler[] handlers) {
-        this(vertx, handlers, new RouteRegister[0]);
-    }
-
-    public Kate(Vertx vertx, KateHandler[] handlers, RouteRegister[] routeRegisters) {
+    public Kate(Vertx vertx, RouteRegister[] routeRegisters) {
         this.vertx = vertx;
-        this.handlers = handlers;
         this.routeRegisters = routeRegisters;
     }
 
@@ -92,19 +74,6 @@ public class Kate {
     protected Future<HttpServer> doStart(String host, int port) {
         Router router = Router.router(vertx);
         preparePreflightRoutes(router);
-        for (KateHandler handler : this.handlers) {
-            router.route(handler.route()).handler(ctx -> {
-                Thread.ofVirtual().name("kate handler thread: " + virtualThreadCounter.getAndIncrement()).uncaughtExceptionHandler(new Thread.UncaughtExceptionHandler() {
-                    @Override
-                    public void uncaughtException(Thread t, Throwable e) {
-                        logger.warn("uncaught exception in thread: (" + t.getName() + "):\n" + ExceptionUtils.getStackTrace(e));
-                        ctx.response().setStatusCode(500).end();
-                    }
-                }).start(() -> {
-                    handler.handle(ctx);
-                });
-            });
-        }
         for (RouteRegister routeRegister : this.routeRegisters) {
             routeRegister.apply(router);
         }
@@ -154,22 +123,22 @@ public class Kate {
 
         TemplateEngine te = JteTemplateEngineFactory.apply();
 
-        KateHandler pageHandler = new SamplePageHandler(te);
-
-        KateHandler sampleHandler = new KateHandler() {
-
+        RouteRegister rr = new RouteRegister() {
             @Override
-            public void handle(RoutingContext ctx) {
-                ctx.response().end("DONE!");
-            }
-
-            @Override
-            public String route() {
-                return "/";
+            void apply(Router router) {
+                router.get("/").handler(ctx -> ctx.response().end("DONE!"));
+                router.get("/page").handler(ctx -> {
+                    Map<String, Object> templateContext = new HashMap<>();
+                    templateContext.put("message", "Hello, Kate.");
+                    ResponseUtils.html(ctx, JteTemplateUtils.merge(te, "sample.jte", templateContext), 200);
+                });
             }
         };
 
-        Kate kate = new Kate(new KateHandler[]{sampleHandler, pageHandler});
+
+        Kate kate = new Kate(new RouteRegister[]{
+                rr
+        });
         kate.start("localhost", 9999);
         System.in.read();
         kate.stop();
